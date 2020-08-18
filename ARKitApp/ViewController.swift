3ef1184b2a28634b937657687e1babe1d0fe37cc
dzeyneptk <zeyneptokcand@gmail.com>
2020-08-18 13:56:42 +0300

    Initial Commit

---
+++ ARKitApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ARKitApp
+//
+//  Created by zeynep tokcan on 18.08.2020.
+//  Copyright © 2020 zeynep tokcan. All rights reserved.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
